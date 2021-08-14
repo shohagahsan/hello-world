72adf91283443ccc71f180153132012318a080c8
Shohag Ahsan <theShohagAhsan@gmail.com>
2021-08-14 19:51:44 +0600

    REACT Forms Chapter Complete

--- src/components/representational/NewBook.js
+++ src/components/representational/NewBook.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NewBook = props => {
-    console.log(props);
-    return (
-        <div>
-            <h1>Entry New Book</h1>
-        </div>
-    );
+class NewBook extends Component {
+    constructor(props) {
+        super(props);
+        this.bookName = React.createRef();
+        this.writer = React.createRef();
+        this.description = React.createRef();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = event => {
+        console.log(this.bookName.current.value);
+        console.log(this.writer.current.value);
+        console.log(this.description.current.value);
+        event.preventDefault();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Entry New Book</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Book Name: </label>
+                    <br />
+                    <input type="text" name="bookName" ref={this.bookName} />
+                    <br />
+                    <label>Writer: </label>
+                    <br />
+                    <input type="text" name="writer" ref={this.writer} />
+                    <br />
+                    <label>Description: </label>
+                    <br />
+                    <textarea name="description" ref={this.description} />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
+
 }
 
 export default NewBook;
